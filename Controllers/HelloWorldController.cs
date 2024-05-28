646a9667f4d5226553a6905c981603061af4e49c
cjohnson <collin.g.johnson@gmail.com>
2024-05-27 23:04:03 -0400

    Finish part 2, add a controller

---
+++ Controllers/HelloWorldController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace ASPNetCoreMVCTutorial.Controllers;
+
+public class HelloWorldController : Controller
+{
+    // 
+    // GET: /HelloWorld/
+    public string Index()
+    {
+        return "This is my default action...";
+    }
+    // 
+    // GET: /HelloWorld/Welcome/ 
+    // Requires using System.Text.Encodings.Web;
+    public string Welcome(string name, int id = 1)
+    {
+        return HtmlEncoder.Default.Encode($"Hello {name}, ID: {id}");
+    }
+}
