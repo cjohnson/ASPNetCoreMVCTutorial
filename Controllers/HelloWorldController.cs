e978b48f8b369fd827f0949d01b494a9c2cbc641
cjohnson <collin.g.johnson@gmail.com>
2024-06-06 21:22:08 -0400

    Finish Part 3, add a view

--- Controllers/HelloWorldController.cs
+++ Controllers/HelloWorldController.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using System.Text.Encodings.Web;
 
 namespace ASPNetCoreMVCTutorial.Controllers;
 
@@ -7,15 +6,17 @@ public class HelloWorldController : Controller
 {
     // 
     // GET: /HelloWorld/
-    public string Index()
+    public IActionResult Index()
     {
-        return "This is my default action...";
+        return View();
     }
     // 
     // GET: /HelloWorld/Welcome/ 
-    // Requires using System.Text.Encodings.Web;
-    public string Welcome(string name, int id = 1)
+    public IActionResult Welcome(string name, int numTimes = 1)
     {
-        return HtmlEncoder.Default.Encode($"Hello {name}, ID: {id}");
+        ViewData["Message"] = "Hello " + name;
+        ViewData["NumTimes"] = numTimes;
+
+        return View();
     }
 }
